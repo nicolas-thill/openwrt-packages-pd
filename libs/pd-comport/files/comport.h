7605f9669d24bc00c8b06bb1dd94bee7c3f59d01
Nicolas Thill <nicolas.thill@gmail.com>
2019-07-19 15:36:32 +0200

    add pd-comport
    PD external to use serial ports
    
    Signed-off-by: Nicolas Thill <nicolas.thill@gmail.com>

---
+++ libs/pd-comport/files/comport.h
@@ -0,0 +1,22 @@
+/*
+ * comport - PD external for unix/windows to use the serial ports
+ *
+ * (c) 1998-2005  Winfried Ritsch
+ * Institute for Electronic Music - Graz
+ *
+ */
+
+#ifndef __PD_COMPORT_H
+#define __PD_COMPORT_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void comport_setup(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __PD_COMPORT_H */
